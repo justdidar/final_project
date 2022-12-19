7cc959b7eb133659f6fbc356e233a86e87fa1a7d
justddar <didar.temirhanov03@gmail.com>
2022-12-20 02:11:51 +0600

    idk4commit

--- script.js
+++ script.js
@@ -2,27 +2,38 @@ let menuBtn = document.querySelector('.htext1');
 let menu = document.querySelector('.login');
 let ex = document.querySelector('.ex');
 let activebtn = document.querySelector('.htext2');
+let menuBtn1 = document.querySelector('.rtext1');
 let search = document.querySelector('.search');
+let searchtext = document.querySelector('.searchtext');
 let butex = document.querySelector('.butex');
 let body = document.querySelector('body');
+let menutext = document.querySelector('.menutext');
+let butex1 = document.querySelector('.butex1');
+let lmenu = document.querySelector('.lmenu');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
-    body.classList.toggle('shadow');
+})
+
+menutext.addEventListener('click', function(){
+    lmenu.classList.toggle('active1');
+})
+
+butex1.addEventListener('click', function(){
+    lmenu.classList.remove('active1');
 })
 
 ex.addEventListener('click', function(){
     menu.classList.remove('active');
-    body.classList.toggle('shadow');
+    
 })
 
 
 activebtn.addEventListener('click', function(){
     search.classList.toggle('active');
-    body.classList.toggle('shadow');
 })
 
 butex.addEventListener('click', function(){
     search.classList.remove('active');
-    body.classList.remove('shadow');
+   
 })
