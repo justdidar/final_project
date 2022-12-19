76b719cb20111d855b9be457c6189e421354278b
justddar <didar.temirhanov03@gmail.com>
2022-12-19 23:50:35 +0600

    idkcommit3

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let menuBtn = document.querySelector('.htext1');
 let menu = document.querySelector('.login');
 let ex = document.querySelector('.ex');
+let activebtn = document.querySelector('.htext2');
+let search = document.querySelector('.search');
+let butex = document.querySelector('.butex');
+let body = document.querySelector('body');
 
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
+    body.classList.toggle('shadow');
 })
 
 ex.addEventListener('click', function(){
     menu.classList.remove('active');
+    body.classList.toggle('shadow');
+})
+
+
+activebtn.addEventListener('click', function(){
+    search.classList.toggle('active');
+    body.classList.toggle('shadow');
+})
+
+butex.addEventListener('click', function(){
+    search.classList.remove('active');
+    body.classList.remove('shadow');
 })
